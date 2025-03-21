83b9310a4f8194c7ca72a7ca03e1c0c8c43f2419
Mostafa Kamal <hiremostafa@gmail.com>
2025-03-22 04:12:44 +0600

    fix: make proto dir and delete existing proto

--- generate.sh
+++ generate.sh
@@ -1,2 +1,8 @@
+# Create output directory if it doesn't exist
+mkdir -p src/proto
+# Clean existing generated files, except .gitkeep
+rm -rf src/proto/*.py src/proto/*.pyc
+# Generate proto files
 python -m grpc_tools.protoc -I=protobuf/ --python_out=src/proto --grpc_python_out=src/proto protobuf/*.proto
-	cd src/proto && sed -i '' 's/^\(import.*pb2\)/from . \1/g' *.py
+# Fix imports to be relative
+cd src/proto && sed -i '' 's/^\(import.*pb2\)/from . \1/g' *.py
