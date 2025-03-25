183f6aeac7551825b6669969a39f98cea53c3f10
Mostafa Kamal <hiremostafa@gmail.com>
2025-03-26 03:37:03 +0600

    perf: add logger for exception

--- src/interceptors/logging.py
+++ src/interceptors/logging.py
@@ -5,18 +5,30 @@ from loguru import logger
 # Configure Loguru
 logger.add("grpc_server.log", rotation="10MB", level="INFO")
 
+
 class LoggingInterceptor(ServerInterceptor):
     def intercept(self, method, request, context, method_name):
-        
-        response = method(request, context)
-        logger.info(
-            "gRPC Request | "
-            f"Method: {method_name} | "
-            f"Request: {request}"
-        )
-        logger.info(
-            "gRPC Response | "
-            f"Method: {method_name} | "
-            f"Response: {response}"
-        )
-        return response
+        try:
+            response = method(request, context)
+            logger.info(
+                "gRPC Request | "
+                f"Method: {method_name} | "
+                f"Request: {request}"
+            )
+            logger.info(
+                "gRPC Response | "
+                f"Method: {method_name} | "
+                f"Response: {response}"
+            )
+            return response
+        except Exception as e:
+            # Log the error
+            logger.error(
+                f"Error in gRPC call | "
+                f"Method: {method_name} | "
+                f"Request: {request} | "
+                f"Error: {str(e)}"
+            )
+ 
+            # Re-raise the exception to maintain the original behavior
+            raise
